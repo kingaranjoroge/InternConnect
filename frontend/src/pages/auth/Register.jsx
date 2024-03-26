f824e5b7ed5ce8c7e5d3a4ec58e6f7bae1654795
kingaranjoroge <briankingara4@gmail.com>
2024-03-27 01:46:59 +0300

    modified styling

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -4,10 +4,10 @@ const Register = () => {
   return (
     <main className="flex flex-row w-screen h-screen">
         <section className="flex items-center justify-center w-1/2 bg-slate-200">
-            <div className="bg-blue-800 font-semibold p-3 rounded-full text-slate-200 hover:bg-blue-600"><Link to="/user-registration">Register as an Attachee / Intern</Link></div>
+            <div className="bg-blue-800 font-semibold p-3 rounded-xl text-slate-200 hover:bg-blue-600"><Link to="/user-registration">Register as an Attachee / Intern</Link></div>
         </section>
         <section className="flex items-center justify-center w-1/2 bg-blue-800">
-            <div className="bg-slate-200 font-semibold p-3 rounded-full text-black hover:bg-slate-400"><Link to="/org-registration">Register as an Organization</Link></div>
+            <div className="bg-slate-200 font-semibold p-3 rounded-xl text-black hover:bg-slate-400"><Link to="/org-registration">Register as an Organization</Link></div>
         </section>
     </main>
   )
