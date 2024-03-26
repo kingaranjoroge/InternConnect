056a8e3fcba79526c8e7627136c811c7a30d2649
kingaranjoroge <briankingara4@gmail.com>
2024-03-27 01:46:33 +0300

    added routes to new pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,21 @@ import Register from './pages/auth/Register'
 import UserRegistration from './pages/auth/UserRegistration'
 import OrgRegistration from './pages/auth/OrgRegistration'
 import Login from './pages/auth/Login'
-import Attachments from './components/attachments/Attachments'
+import Attachments from './pages/Attachments'
+import Home from './pages/Home'
+import Application from './pages/Application'
 
 const App = () => {
   return (
     <Routes>
-      <Route index element={<Attachments />} />
+      <Route index element={<Home />} />
       <Route path='admin' element={<Admin />} />
       <Route path='sign-up' element={<Register />} />
       <Route path='user-registration' element={<UserRegistration />} />
       <Route path='org-registration' element={<OrgRegistration />} />
       <Route path='sign-in' element={<Login />} />
+      <Route path='attachments' element={<Attachments />} />
+      <Route path="application" element={<Application />} />
     </Routes>
   )
 }
