a49810e779e89c6998508a51616187e6575e8e34
kingaranjoroge <briankingara4@gmail.com>
2024-04-03 12:01:08 +0300

    added home page navigation

--- frontend/src/components/navigation/AttachmentsNavbar.jsx
+++ frontend/src/components/navigation/AttachmentsNavbar.jsx
@@ -11,7 +11,8 @@ const AttachmentsNavbar = ({ onSearch }) => {
   return (
     <nav className="flex flex-row items-center justify-between bg-blue-800 p-2 h-[12.5vh]">
       <h1 className="font-bold text-2xl text-slate-200 pl-4">InternConnect</h1>
-      <div className="flex flex-row gap-4 pr-8">
+      <div className="flex flex-row gap-8 pr-8">
+        <p className="font-bold text-slate-200 text-base pt-2 hover:text-slate-300"><a href="/">Home</a></p>
         <input
           type="text"
           placeholder="Search attachments..."
