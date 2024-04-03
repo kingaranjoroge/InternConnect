c4d56312a7a424d2cbbdafb2364be28a483f910e
kingaranjoroge <briankingara4@gmail.com>
2024-04-03 11:58:50 +0300

    added logout page and auth guarding for admin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Admin from "./pages/Admin"
 import Register from './pages/auth/Register'
 import UserRegistration from './pages/auth/UserRegistration'
@@ -7,20 +7,24 @@ import Login from './pages/auth/Login'
 import Attachments from './pages/Attachments'
 import Home from './pages/Home'
 import Application from './pages/Application'
+import Logout from './pages/auth/Logout'
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+
   return (
     <Routes>
       <Route index element={<Home />} />
-      <Route path='admin' element={<Admin />} />
+      <Route path='admin' element={user && user.role === 'admin' ? <Admin /> : <Navigate to="/" />} />
       <Route path='sign-up' element={<Register />} />
       <Route path='user-registration' element={<UserRegistration />} />
       <Route path='org-registration' element={<OrgRegistration />} />
       <Route path='sign-in' element={<Login />} />
       <Route path='attachments' element={<Attachments />} />
       <Route path="application" element={<Application />} />
+      <Route path="sign-out" element={<Logout />} />
     </Routes>
   )
 }
 
-export default App
+export default App;
