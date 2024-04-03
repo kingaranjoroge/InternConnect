af45cef11113a1cd6df653a2a5199523908bc983
kingaranjoroge <briankingara4@gmail.com>
2024-04-03 12:02:21 +0300

    added conditional signout and admin page navigation

--- frontend/src/components/navigation/MainNavbar.jsx
+++ frontend/src/components/navigation/MainNavbar.jsx
@@ -1,13 +1,24 @@
+import React from 'react';
+
 const MainNavbar = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+
   return (
     <nav className="flex flex-row items-center justify-between bg-blue-800 p-2 h-[12.5vh]">
         <h1 className="font-bold text-2xl text-slate-200 pl-4">InternConnect</h1>
         <div className="flex flex-row gap-4 pr-8">
+            {user && user.role === 'admin' && (
+              <p className="font-bold text-slate-200 text-base hover:text-slate-300"><a href="/admin">Admin</a></p>
+            )}
             <p className="font-bold text-slate-200 text-base hover:text-slate-300"><a href="/attachments">Attachments</a></p>
-            <p className="font-bold text-slate-200 text-base hover:text-slate-300"><a href="/sign-in">Sign-in</a></p>
+            {user ? (
+              <p className="font-bold text-slate-200 text-base hover:text-slate-300"><a href="/sign-out">Sign-out</a></p>
+            ) : (
+              <p className="font-bold text-slate-200 text-base hover:text-slate-300"><a href="/sign-in">Sign-in</a></p>
+            )}
         </div>
     </nav>
   )
 }
 
-export default MainNavbar
+export default MainNavbar;
