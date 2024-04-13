c09d4ae744e47463232dc86f2a1538df9fe61834
kingaranjoroge <briankingara4@gmail.com>
2024-04-13 23:51:06 +0300

    added attachments and org admin navbar

--- frontend/src/pages/OrgAdmin.jsx
+++ frontend/src/pages/OrgAdmin.jsx
@@ -1,17 +1,17 @@
-import AllAttachments from "../components/admin/attachments/AllAttachments"
+import Attachments from "../components/org-admin/Attachments"
 import CreateAttachment from "../components/admin/attachments/CreateAttachment"
 import UpdateAttachment from "../components/admin/attachments/UpdateAttachment"
 import FetchAttachment from "../components/admin/attachments/FetchAttachment"
 import DeleteAttachment from "../components/admin/attachments/DeleteAttachment"
-import MainNavbar from "../components/navigation/MainNavbar"
+import OrgAdminNavbar from "../components/navigation/OrgAdminNavbar"
 
 const OrgAdmin = () => {
   return (
     <main className="flex flex-col">
-        <MainNavbar />
+        <OrgAdminNavbar />
         <section className="flex flex-row gap-4 px-4 py-4" style={{ minHeight: '90vh' }}>
             <section className="bg-slate-200 w-1/2 rounded overflow-auto">
-                <AllAttachments />
+                <Attachments />
             </section>
             <section className="flex flex-col gap-6 bg-slate-200 w-1/2 rounded p-4">
                 <CreateAttachment />
