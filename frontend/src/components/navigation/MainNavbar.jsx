b95b6667cdb038156f7fb09e6c23d7e7d493d8ba
kingaranjoroge <briankingara4@gmail.com>
2024-04-13 23:49:31 +0300

    added org-admin route

--- frontend/src/components/navigation/MainNavbar.jsx
+++ frontend/src/components/navigation/MainNavbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const MainNavbar = () => {
   const user = JSON.parse(localStorage.getItem('user'));
+  const org = JSON.parse(localStorage.getItem('org'));
 
   return (
     <nav className="flex flex-row items-center justify-between bg-blue-800 p-2 h-[12.5vh]">
@@ -10,8 +11,11 @@ const MainNavbar = () => {
             {user && user.role === 'admin' && (
               <p className="font-bold text-slate-200 text-base hover:text-slate-300"><a href="/admin">Admin</a></p>
             )}
+            {org && org.role === 'org' && (
+              <p className="font-bold text-slate-200 text-base hover:text-slate-300"><a href="/org-admin">OrgAdmin</a></p>
+            )}
             <p className="font-bold text-slate-200 text-base hover:text-slate-300"><a href="/attachments">Attachments</a></p>
-            {user ? (
+            {user || org ? (
               <p className="font-bold text-slate-200 text-base hover:text-slate-300"><a href="/sign-out">Sign-out</a></p>
             ) : (
               <p className="font-bold text-slate-200 text-base hover:text-slate-300"><a href="/sign-in">Sign-in</a></p>
