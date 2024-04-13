0b8c5af0e4273367b9b060f67f23a93a1f1e7da8
kingaranjoroge <briankingara4@gmail.com>
2024-04-13 23:52:11 +0300

    created org login page

---
+++ frontend/src/pages/auth/OrgLogin.jsx
@@ -0,0 +1,54 @@
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    const email = event.target[0].value;
+    const password = event.target[1].value;
+
+    const response = await axios.post('http://localhost:3000/organizations/validate', { email, password });
+    if (response.data) {
+      // organization is valid, redirect to the "org-admin" page
+      localStorage.setItem('org', JSON.stringify(response.data));
+      navigate("/org-admin");
+    } else {
+      // Org is not valid, show an error message
+      alert('Invalid email or password');
+    }
+  };
+
+  return (
+    <main className="flex items-center justify-center w-screen h-screen">
+      <form className="flex flex-col gap-4 w-1/3" onSubmit={handleLogin}>
+        <div className="flex items-center justify-center">
+          <h1 className="text-blue-800 font-bold text-lg">Login</h1>
+        </div>
+        <div className="flex flex-col gap-4">
+          <input
+            type="email"
+            placeholder="email"
+            className="input input-bordered"
+            required
+          />
+          <input
+            type="password"
+            placeholder="password"
+            className="input input-bordered"
+            required
+          />
+          <p className="place-self-center">Don't have an account? <a className="text-blue-800" href="/sign-up">Register</a></p>
+        </div>
+        <div className="flex items-center justify-center">
+          <button type="submit" className="btn bg-blue-800 w-1/2 text-slate-200 hover:bg-blue-950">
+            Login
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default Login;
