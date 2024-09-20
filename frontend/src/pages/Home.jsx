3f2537dab8961e54d6d421f81a695486547ca380
kingaranjoroge <briankingara4@gmail.com>
2024-09-20 23:29:15 +0300

    change text size

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,13 +17,13 @@ const Home = () => {
         <MainNavbar />
         <section className="flex flex-col items-center justify-center gap-4 h-[87.5vh] overflow-auto">
             <div>
-                <p className="text-blue-800 font-bold text-xs sm:text-xl md:text-2xl lg:text-4xl">Unlock endless internship & attachment possibilities with <br /> our student-centric attachment platform today!
+                <p className="text-blue-800 font-bold text-sm sm:text-xl md:text-2xl lg:text-4xl">Unlock endless internship & attachment possibilities with <br /> our student-centric attachment platform today!
                 </p>
             </div>
             <div className="flex flex-col items-center justify-center">
-                <p className="text-[8px] sm:text-sm md:text-base font-semibold text-gray-500">Empower your future with our comprehensive software platform, connecting students to</p>
-                <p className="text-[8px] sm:text-sm md:text-base font-semibold text-gray-500">a vast array of internship and attachment opportunities, ensuring you</p>
-                <p className="text-[8px] sm:text-sm md:text-base font-semibold text-gray-500">find the perfect match to kickstart your career journey.</p>
+                <p className="text-[10px] sm:text-sm md:text-base font-semibold text-gray-500">Empower your future with our comprehensive software platform, connecting students to</p>
+                <p className="text-[10px] sm:text-sm md:text-base font-semibold text-gray-500">a vast array of internship and attachment opportunities, ensuring you</p>
+                <p className="text-[10px] sm:text-sm md:text-base font-semibold text-gray-500">find the perfect match to kickstart your career journey.</p>
             </div>
             <div className="flex flex-row items-center justify-between gap-4 pt-1">
                 <button className="btn bg-blue-800 text-slate-200 px-4 sm:px-16 hover:bg-blue-950" onClick={handleRegisterNavigate}>Register</button>
